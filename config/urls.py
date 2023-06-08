e1da204397201efbee9a8fe3649dc3bd8b920a17
EXTRAS12 <extra-kent@mail.ru>
2023-06-08 16:48:19 +0300

    day3 html form register, login

--- config/urls.py
+++ config/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    # path('', include('main.urls')),
-    # path('search/', include('search.urls')),
-    # path('', include('account.urls')),
 ]
 
 urlpatterns += i18n_patterns(
