e40b212e87c6b65a343affdc84d03eb6817d1ce9
EXTRAS12 <extra-kent@mail.ru>
2023-06-15 18:28:00 +0300

    refactoring html

--- main/views.py
+++ main/views.py
@@ -12,6 +12,10 @@ class FrontPage(ListView):
     def get_queryset(self):
         return Post.objects.filter(is_published=True)
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recomended'] = Post.objects.filter(is_published=True).order_by('views')
+        return context
 
 class PostList(ListView):
     model = Post
